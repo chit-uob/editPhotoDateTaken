ac1e86aef28060f6d1bc4d7ca00dcb10bc0275bd
cleeao <cleeao@connect.ust.hk>
2022-04-22 12:51:52 +0100

    added exception handling for reading config

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import configparser
+import traceback
+from pathlib import Path
 
 # Get the source and destination folder
 # todo when use rename sample_config.ini to config.ini
 config = configparser.ConfigParser()
 config.read('config.ini')
-SOURCE_FOLDER = config['MAIN']['sourceFolder']
-DESTINATION_FOLDER = config['MAIN']['destinationFolder']
+try:
+    SOURCE_FOLDER = Path(config['MAIN']['sourceFolder'])
+    DESTINATION_FOLDER = Path(config['MAIN']['destinationFolder'])
+except Exception:
+    print("Faced exception when parsing the config:")
+    print(traceback.format_exc())
+    raise SystemExit(-1)
 
 
 def main():
-    print(SOURCE_FOLDER)
-    print(DESTINATION_FOLDER)
+    print(f"The source folder is {SOURCE_FOLDER}")
+    print(f"The destination folder is {DESTINATION_FOLDER}")
 
 
 if __name__ == '__main__':
